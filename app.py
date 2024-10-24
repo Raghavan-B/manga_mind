ee76ce5cfd6c873fbcaab1d13902db5fcfe8033b
RAGHAVAN B <122632349+Raghavan-B@users.noreply.github.com>
2024-10-24 23:30:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,80 +6,18 @@ import warnings
 import base64
 from PIL import Image,ImageFilter
 
-# image  = Image.open("manga_background.jpg")
-# blurred_img  = image.filter(ImageFilter.GaussianBlur())
-# blurred_img.save("bg_blurred.png")
-# image_base64 = base64.b64encode(blurred_img).decode()
 warnings.filterwarnings("ignore")
 from utils import convert_genre_to_str,recommend,display_results
 
 st.set_page_config(layout="wide")
 
 ##Loading embeddings, Faiss index, Dataset
-data = pd.read_csv("dataset\manga_dataset.csv")
-embeddings = np.load("models\embeddings_pca.npy")
+data = pd.read_csv("dataset/manga_dataset.csv")
+embeddings = np.load("models/embeddings_pca.npy")
 index = faiss.read_index("models/faiss_index.bin")
-# def add_bg_from_local(image_file):
-#     with open(image_file, "rb") as image_file:
-#         encoded_string = base64.b64encode(image_file.read())
-#     st.markdown(
-#     f"""
-#     <style>
-#     .stApp {{
-#         background-image: url(data:image/{"gif"};base64,{encoded_string.decode()});
-#         background-size: cover
-#     }}
-#     </style>
-#     """,
-#     unsafe_allow_html=True
-#     )
-
-
-# add_bg_from_local("manga_background.jpg")   
-
-
-# # Function to encode the image file as base64
-# def get_base64_image(image_path):
-#     with open(image_path, "rb") as file:
-#         data = file.read()
-#     return base64.b64encode(data).decode()
-
-# # Path to your background image
-# image_path = "bg_blurred.png"
-
-# # Convert the image to base64
-# image_base64 = get_base64_image(image_path)
-
-# st.markdown(
-#     f"""
-#     <style>
-#     .stApp {{
-#         background-image: url("data:image/png;base64,{image_base64}");
-#         background-size: cover;
-#         background-position: center;
-#         background-repeat: no-repeat;
-#     }}
-#     </style>
-#     """,
-#     unsafe_allow_html=True
-# )
-
 
 
 data["genres"] = data["genres"].apply(convert_genre_to_str)
-#Creating a class container where there will be a sction whose background will be white
-# st.markdown("""
-#     <style>
-#     .container {
-#         padding: 16px;
-#         border: 1px solid #ddd;
-#         border-radius: 5px;
-#         background-color: #f5f5f5;
-#         margin-bottom: 16px;
-#     }
-#     </style>
-# """, unsafe_allow_html=True)
-
 st.write('<h1 style = "color:black;">Manga Mind - An AI Based Recommender System',unsafe_allow_html=True)
 titles = data["title"].tolist()
 
